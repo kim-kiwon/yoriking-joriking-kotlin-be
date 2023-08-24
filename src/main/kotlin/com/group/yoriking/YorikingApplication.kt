ceedf8dca5a72a3137eb7815abe0d50c57eff970
KG <kimkiwonn@gmail.com>
2023-08-24 13:27:01 +0900

    Add 프로젝트 생성

---
+++ src/main/kotlin/com/group/yoriking/YorikingApplication.kt
@@ -0,0 +1,11 @@
+package com.group.yoriking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YorikingApplication
+
+fun main(args: Array<String>) {
+    runApplication<YorikingApplication>(*args)
+}
---
+++ src/test/kotlin/com/group/yoriking/YorikingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.group.yoriking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YorikingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
